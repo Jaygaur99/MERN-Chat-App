7767edf8a54791af9e1a93aef21df2ab78a7a145
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-10-28 15:01:04 +0530

    Added Signup and Login Page Components Connected MongoDB to the Application

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data.js");
+const connectDB = require("./config/db.js");
+const colors = require("colors");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is Running");
@@ -20,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,60 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabPanel,
+  TabPanels,
+  TabList,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="50%" centerContent>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        p={3}
+        bg={"white"}
+        width="100%"
+        margin="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily={"Roboto"} color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign-up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
