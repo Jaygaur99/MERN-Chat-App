8ea60a6aa81129513de3372207b8f11c3b9e2ef0
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-10-29 11:23:56 +0530

    Created routes and controllers related to Chat

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers.js");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers.js");
+const { protect } = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
